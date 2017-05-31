7237af5d495b7f783b5e8734b20b512ac39a809e
Iris Kim <iris.val19@gmail.com>
2017-05-31 02:13:59 -0400

    Initial Commit.

---
+++ graph.py
@@ -0,0 +1,50 @@
+from collections import defaultdict, OrderedDict
+import json
+import copy
+
+
+# directed graph repped thru adjacency list
+class Person:
+	def __init__(self, name, idNum, spouse=None, ex_spouse=None, father=None, mother=None, children=None):
+		self.name = name
+		self.idNum = idNum
+		self.relationships = {"spouse": spouse, "ex_spouse": ex_spouse, "father": father, "mother": mother, "children": children}
+
+
+	def __repr__(self):
+		return self.name
+
+
+class Graph:
+	def __init__(self):
+		self.graph = defaultdict(list)
+
+	def addRelationship(self, u, v):
+		if v is not None:
+			self.graph[u].append(v)
+
+	def shortestPath(self, u, v):
+		queue = [[u]]
+		visited = []
+
+		while (len(queue) > 0):
+			path = queue.pop(0)
+			node = path[-1]
+			
+			if node not in visited:
+				relations = self.graph[node]
+				
+				for relation in relations:
+					new_path = copy.deepcopy(path)
+					new_path.append(relation)
+					queue.append(new_path)
+					
+					if relation == v:
+						return new_path
+				visited.append(node)
+		return "path does not exist"
+
+		
+	def __repr__(self):
+		return str(self.graph)
+
---
+++ main.py
@@ -0,0 +1,40 @@
+from collections import defaultdict, OrderedDict
+import json
+import copy
+from graph import Person, Graph 
+
+
+
+def main():
+	with open('windsors.json', 'r') as f:
+	 	data = json.load(f, object_pairs_hook=OrderedDict)
+
+	graph = Graph()
+	family = data['Windsors']
+	relations = ['spouse', 'ex_spouse', 'mother', 'father', 'children']
+	people = []
+
+	for member in family:
+		person = Person(member, family[member]["id"], family[member]["spouse"], family[member]["ex_spouse"], family[member]["father"], family[member]["mother"], family[member]["children"])
+		people.append(person)
+
+	for person in people:
+		for relation in person.relationships:
+			if relation is not None:
+				if isinstance(person.relationships[relation], list):
+					for otherMemberId in person.relationships[relation]:
+						graph.addRelationship(person, people[otherMemberId])
+				else:
+					otherMemberId = person.relationships[relation]
+					if otherMemberId is not None:
+						graph.addRelationship(person, people[otherMemberId])
+
+
+	connection = graph.shortestPath(people[0], people[21])
+
+	statement = "The most direct line of relation between " + str(people[0]) + " and " + str(people[21]) + " is:"
+	for person in connection:
+		statement += "\n\t--> " + str(person)
+	print(statement)
+
+main()
